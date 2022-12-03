57e1383f5da3183baac85f92239f12adbce12dcc
Samuel Phillips <codedchrysalis@gmail.com>
2022-12-02 17:55:40 -0800

    Adjusted price on Services Page

--- services.php
+++ services.php
@@ -1,134 +1,139 @@
-<?php include "includes/header.php";?>
+<?php include "includes/header.php"; ?>
 
 <div class="services">
 
-<div class="serviceCard service-1">
+    <div class="serviceCard service-1">
 
-<h2>WordPress Design & Development</h2>
+        <h2>WordPress Design & Development</h2>
 
-<div class="service_type">
+        <div class="service_type">
 
-<h3>Basic</h3><h4>$1000 - $1500</h4>
+            <h3>Basic</h3>
+            <h4>$1000 - $1500</h4>
+
+        </div>
+
+        <ul>
+
+            <li>Audience, Issue, & Approach Analysis</li>
+            <li>Customized Template Design</li>
+            <li>Elementor Development</li>
+            <li>WordPress CMS</li>
+            <li>2 Editing Sessions</li>
+            <li>1-3 Pages</li>
+
+        </ul>
+
+        <button>learn more</button>
+
+    </div>
+
+    <div class="serviceCard service-2">
+
+        <h2>WordPress Design & Development</h2>
+
+        <div class="service_type">
+
+            <h3>Basic</h3>
+            <h4>$1000 - $1500</h4>
+
+        </div>
+
+        <ul>
+
+            <li>Audience, Issue, & Approach Analysis</li>
+            <li>Customized Template Design</li>
+            <li>Elementor Development</li>
+            <li>WordPress CMS</li>
+            <li>2 Editing Sessions</li>
+            <li>1-3 Pages</li>
+
+        </ul>
+
+        <button>learn more</button>
+
+    </div>
+
+    <div class="serviceCard service-3">
+
+        <h2>WordPress Design & Development</h2>
+
+        <div class="service_type">
+
+            <h3>Basic</h3>
+            <h4>$1000 - $1500</h4>
+
+        </div>
+
+        <ul>
+
+            <li>Audience, Issue, & Approach Analysis</li>
+            <li>Customized Template Design</li>
+            <li>Elementor Development</li>
+            <li>WordPress CMS</li>
+            <li>2 Editing Sessions</li>
+            <li>1-3 Pages</li>
+
+        </ul>
+
+        <button>learn more</button>
+
+    </div>
+
+    <div class="serviceCard service-4">
+
+        <h2>WordPress Design & Development</h2>
+
+        <div class="service_type">
+
+            <h3>Basic</h3>
+            <h4>$1000 - $1500</h4>
+
+        </div>
+
+        <ul>
+
+            <li>Audience, Issue, & Approach Analysis</li>
+            <li>Customized Template Design</li>
+            <li>Elementor Development</li>
+            <li>WordPress CMS</li>
+            <li>2 Editing Sessions</li>
+            <li>1-3 Pages</li>
+
+        </ul>
+
+        <button>learn more</button>
+
+    </div>
+
+    <div class="serviceCard service-5">
+
+        <h2>WordPress Design & Development</h2>
+
+        <div class="service_type">
+
+            <h3>Basic</h3>
+            <h4>$1000 - $1500</h4>
+
+        </div>
+
+        <ul>
+
+            <li>Audience, Issue, & Approach Analysis</li>
+            <li>Customized Template Design</li>
+            <li>Elementor Development</li>
+            <li>WordPress CMS</li>
+            <li>2 Editing Sessions</li>
+            <li>1-3 Pages</li>
+
+        </ul>
+
+        <button>learn more</button>
+
+    </div>
 
 </div>
 
-<ul>
-
-<li>Audience, Issue, & Approach Analysis</li>
-<li>Customized Template Design</li>
-<li>Elementor Development</li>
-<li>WordPress CMS</li>
-<li>2 Editing Sessions</li>
-<li>1-3 Pages</li>
-
-</ul>
-
-<button>learn more</button>
-
-</div>
-
-<div class="serviceCard service-2">
-
-<h2>WordPress Design & Development</h2>
-
-<div class="service_type">
-
-<h3>Basic</h3><h4>$1000 - $1500</h4>
-
-</div>
-
-<ul>
-
-<li>Audience, Issue, & Approach Analysis</li>
-<li>Customized Template Design</li>
-<li>Elementor Development</li>
-<li>WordPress CMS</li>
-<li>2 Editing Sessions</li>
-<li>1-3 Pages</li>
-
-</ul>
-
-<button>learn more</button>
-
-</div>
-
-<div class="serviceCard service-3">
-
-<h2>WordPress Design & Development</h2>
-
-<div class="service_type">
-
-<h3>Basic</h3><h4>$1000 - $1500</h4>
-
-</div>
-
-<ul>
-
-<li>Audience, Issue, & Approach Analysis</li>
-<li>Customized Template Design</li>
-<li>Elementor Development</li>
-<li>WordPress CMS</li>
-<li>2 Editing Sessions</li>
-<li>1-3 Pages</li>
-
-</ul>
-
-<button>learn more</button>
-
-</div>
-
-<div class="serviceCard service-4">
-
-<h2>WordPress Design & Development</h2>
-
-<div class="service_type">
-
-<h3>Basic</h3><h4>$1000 - $1500</h4>
-
-</div>
-
-<ul>
-
-<li>Audience, Issue, & Approach Analysis</li>
-<li>Customized Template Design</li>
-<li>Elementor Development</li>
-<li>WordPress CMS</li>
-<li>2 Editing Sessions</li>
-<li>1-3 Pages</li>
-
-</ul>
-
-<button>learn more</button>
-
-</div>
-
-<div class="serviceCard service-5">
-
-<h2>WordPress Design & Development</h2>
-
-<div class="service_type">
-
-<h3>Basic</h3><h4>$1000 - $1500</h4>
-
-</div>
-
-<ul>
-
-<li>Audience, Issue, & Approach Analysis</li>
-<li>Customized Template Design</li>
-<li>Elementor Development</li>
-<li>WordPress CMS</li>
-<li>2 Editing Sessions</li>
-<li>1-3 Pages</li>
-
-</ul>
-
-<button>learn more</button>
-
-</div>
-
-</div>
 
 
-
-<?php include "includes/footer.php";?>
+<?php include "includes/footer.php"; ?>
