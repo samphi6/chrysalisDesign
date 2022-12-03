4a0389d1126899240e26811bc5d64aab7ace01f6
Samuel Phillips <codedchrysalis@gmail.com>
2022-12-02 17:56:17 -0800

    Updated date_default in config file

--- includes/chrysalis-config.php
+++ includes/chrysalis-config.php
@@ -1,6 +1,6 @@
 <?php
 
-date_default_timezone_get('America/ Los_Angeles');
+date_default_timezone_set('America/ Los_Angeles');
 
 define('PAGE_TITLE', basename($_SERVER['PHP_SELF']));
 
@@ -36,14 +36,38 @@ switch (PAGE_TITLE) {
 
     case 'contactme.php':
 
-        $title = "Contact Samuel";
+        $title = "Contact";
         $pageID = "Contact Samuel";
 
         break;
+
+    default:
+
+        $title = PAGE_TITLE;
+        $pageID = "Go Beyond";
 }
 
 $nav1['stp.php'] = "Samuel Phillps";
 $nav1['designs.php'] = "Design";
 $nav1['services.php'] = "Services";
 $nav1['cases.php'] = "Cases";
-$nav1['contactme.php'] = "Contact Samuel";
+$nav1['contactme.php'] = "Contact";
+
+function makeLinks($nav1)
+
+{
+
+    $myReturn = '';
+
+    foreach ($nav1 as $url => $text) {
+
+        if ($url == PAGE_TITLE) {
+
+            $myReturn .= "<li><a class=\"selected\" href=\"$url\">$text</a></li>";
+        } else {
+
+            $myReturn .= "<li><a href=\"$url\">$text</a></li>";
+        }
+    }
+    return $myReturn;
+}
