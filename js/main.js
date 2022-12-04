29f50ef71fa83dde1b273094590d9b8d12f156a0
Samuel Phillips <codedchrysalis@gmail.com>
2022-12-04 10:27:53 -0800

    OnScroll animation JS added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 // SCRIPTS FOR CHRYSALIS D&D //
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+const observer = new IntersectionObserver((entries) => {
+
+entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+    } else {
+        entry.target.classList.remove('show');
+    }
+});
+});
