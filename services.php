e37bfae72951291aa3ffa067b4b595d6d76e21d4
Samuel Phillips <codedchrysalis@gmail.com>
2022-12-05 11:07:49 -0800

    Updated service cards

--- services.php
+++ services.php
@@ -9,7 +9,7 @@
         <div class="service_type">
 
             <h3>Basic</h3>
-            <h4>$1000 - $1500</h4>
+            <h4>$1,000 - $1,500</h4>
 
         </div>
 
@@ -34,8 +34,8 @@
 
         <div class="service_type">
 
-            <h3>Basic</h3>
-            <h4>$1000 - $1500</h4>
+            <h3>Standard</h3>
+            <h4>$1,500 - $2,000</h4>
 
         </div>
 
@@ -46,7 +46,7 @@
             <li>Elementor Development</li>
             <li>WordPress CMS</li>
             <li>2 Editing Sessions</li>
-            <li>1-3 Pages</li>
+            <li>3-5 Pages</li>
 
         </ul>
 
@@ -56,23 +56,23 @@
 
     <div class="serviceCard service-3">
 
-        <h2>WordPress Design & Development</h2>
+        <h2>Custom Design & Development</h2>
 
         <div class="service_type">
 
-            <h3>Basic</h3>
-            <h4>$1000 - $1500</h4>
+            <h3>Advanced</h3>
+            <h4>$3,000 - $4,000</h4>
 
         </div>
 
         <ul>
 
+            <li>Original Desings w/ Figma or XD</li>
             <li>Audience, Issue, & Approach Analysis</li>
-            <li>Customized Template Design</li>
             <li>Elementor Development</li>
             <li>WordPress CMS</li>
-            <li>2 Editing Sessions</li>
-            <li>1-3 Pages</li>
+            <li>3 Editing Sessions</li>
+            <li>3-5 Pages</li>
 
         </ul>
 
@@ -82,23 +82,25 @@
 
     <div class="serviceCard service-4">
 
-        <h2>WordPress Design & Development</h2>
+        <h2>Custom Design & Development</h2>
 
         <div class="service_type">
 
-            <h3>Basic</h3>
-            <h4>$1000 - $1500</h4>
+            <h3>A1</h3>
+            <h4>$4,000 - $5,000</h4>
 
         </div>
 
         <ul>
 
             <li>Audience, Issue, & Approach Analysis</li>
-            <li>Customized Template Design</li>
+            <li>Original Desings w/ Figma or XD</li>
             <li>Elementor Development</li>
-            <li>WordPress CMS</li>
-            <li>2 Editing Sessions</li>
-            <li>1-3 Pages</li>
+            <li>WordPress CMS (recommended)</li>
+            <li>MailChimp Service Integration (optional)</li>
+            <li>3 Editing Sessions</li>
+            <li>6-8 Pages</li>
+            <li>3 Usability Tests</li>
 
         </ul>
 
@@ -108,23 +110,27 @@
 
     <div class="serviceCard service-5">
 
-        <h2>WordPress Design & Development</h2>
+        <h2>Custom Design & Development</h2>
 
         <div class="service_type">
 
-            <h3>Basic</h3>
-            <h4>$1000 - $1500</h4>
+            <h3>A2</h3>
+            <h4>$5,000 - $8,000</h4>
 
         </div>
 
         <ul>
 
             <li>Audience, Issue, & Approach Analysis</li>
-            <li>Customized Template Design</li>
-            <li>Elementor Development</li>
-            <li>WordPress CMS</li>
-            <li>2 Editing Sessions</li>
-            <li>1-3 Pages</li>
+            <li>Original Desings w/ Figma or XD</li>
+            <li>Elementor or PHP Development</li>
+            <li>WordPress CMS (recommended)</li>
+            <li>MailChimp Service Integration (optional)</li>
+            <li>3 Editing Sessions</li>
+            <li>Pages Negotiable</li>
+            <li>Usability Testing</li>
+            <li>15 Minute Updates Instrustional Video</li>
+            <li>2 Edits After Project Completion (30 day experation)</li>
 
         </ul>
 
