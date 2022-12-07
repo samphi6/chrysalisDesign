d0a540b7b83efd7900dcb57e41d7d4e21bb7b92d
Samuel Phillips <codedchrysalis@gmail.com>
2022-12-07 00:17:47 -0800

    added beginning of case cards

--- cases.php
+++ cases.php
@@ -1,3 +1,55 @@
 <?php include "includes/header.php"; ?>
 
+<div class="case-container">
+
+    <div class="case">
+
+        <img src="img/standoffDrawer.png" alt="Korochka Mobile Mockups">
+
+        <div class="case-descr">
+
+            <h2>Standoff: Book Release New Design</h2>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti fuga doloribus voluptates dolore. Quis vitae, aliquam incidunt corrupti, cupiditate dolorem quasi minus tempora molestias a ipsam necessitatibus quaerat, asperiores ipsa cum perspiciatis atque saepe sequi.</p>
+
+        </div>
+
+        <button>learn more</button>
+
+    </div>
+
+    <div class="case">
+
+        <img src="img/tcfw.png" alt="Korochka Mobile Mockups">
+
+        <div class="case-descr">
+
+            <h2>Transforming Cities from Within Resdesign</h2>
+
+            <p>Quo facilis quas explicabo natus officiis, corporis repellendus perferendis voluptas alias corrupti quidem expedita fugit dolores itaque minus, odit sunt illum temporibus sed error. Id minima numquam autem neque eos praesentium, dignissimos aliquam a cupiditate omnis possimus quos iste qui? Dignissimos, sunt maiores. Fuga, nemo.</p>
+
+        </div>
+
+        <button>learn more</button>
+
+    </div>
+
+    <div class="case">
+
+        <img src="img/korochkaMocks.jpg" alt="Korochka Mobile Mockups">
+
+        <div class="case-descr">
+
+            <h2>Korochka Tavern New Design</h2>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti fuga doloribus voluptates dolore. Quis vitae, aliquam incidunt corrupti, cupiditate dolorem quasi minus tempora molestias a ipsam necessitatibus quaerat, asperiores ipsa cum perspiciatis atque saepe sequi.</p>
+
+        </div>
+
+        <button>learn more</button>
+
+    </div>
+
+</div>
+
 <?php include "includes/footer.php"; ?>
