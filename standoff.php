75cae5a1b858710e15a67dce09329ed2642f41d4
Samuel Phillips <codedchrysalis@gmail.com>
2022-12-10 11:30:38 -0800

    added standoff img for mock layout

---
+++ standoff.php
@@ -0,0 +1,25 @@
+<?php include "includes/header.php"; ?>
+
+
+<h1>THIS MY FUCKIN' WORLD'</h1>
+
+<div class="standoff-left">
+
+    <img src="img/standoffMobile1.png" alt="Book Launch Website for Bruce McIvor">
+
+</div>
+
+<div class="standoff-right">
+
+    <img src="img/standoffTabletMenu.png" alt="Tablet Menu for Bruce McIvor Book Launch">
+
+</div>
+
+<div class="standoff-left">
+
+    <img src="img/standoffAbout.png" alt="Book Launch Website for Bruce McIvor">
+
+</div>
+
+
+<?php include "includes/footer.php"; ?>
