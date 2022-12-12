4ba84eb79ebae85f63c9a78696e68d3ff2db9a65
Samuel Phillips <codedchrysalis@gmail.com>
2022-12-12 12:11:11 -0800

    added animation script

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+// SCRIPTS FOR ONSCROLL ANIMATION // 
+
+const observer = new IntersectionObserver((entries) => {
+
+entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+    } else {
+        entry.target.classList.remove('show');
+    }
+});
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
 //SCRIPTS FOR MODAL ACTIVATION //
 
 const openModalButtons = document.querySelectorAll('[data-modal-target]')
