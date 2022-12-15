bd4105cb1d960189006b0c54b000fe42e059a382
Samuel Phillips <codedchrysalis@gmail.com>
2022-12-15 14:51:03 -0800

    started Standoff design page

--- standoff.php
+++ standoff.php
@@ -1,23 +1,26 @@
 <?php include "includes/header.php"; ?>
 
-
-<h1>THIS MY FUCKIN' WORLD'</h1>
-
 <div class="standoff-left">
 
-    <img src="img/standoffMobile1.png" alt="Book Launch Website for Bruce McIvor">
+    <img class="hidden-left" src="img/standoffMobile1.png" alt="Book Launch Website for Bruce McIvor">
+
+    <p class="hidden">Chrysalis design and development takes careful steps to discover the needs of your site ensuring optimal functionality and an aesthetic that will consistently serve a positive user experience. Finding your audience is a priority! Building a space that keeps visitors clicking through your content is our goal. Lifting your vision with the available templates on WordPress will be the beginning of the journey to raising your platform on the web to the next level.</p>
 
 </div>
 
-<div class="standoff-right">
+<div class="standoff-right hidden-right">
 
-    <img src="img/standoffTabletMenu.png" alt="Tablet Menu for Bruce McIvor Book Launch">
+    <p class="hidden">We guarantee, by the end of the design process that our clients are empowered with the magic of Dreamhost and the WordPress content management systems various advances in tech will have you updating up to three pages hassle free leaving you with the benefit of a budget unscathed by future third-party site management.</p>
+
+    <img class="hidden-right" src="img/standoffTabletMenu.png" alt="Tablet Menu for Bruce McIvor Book Launch">
 
 </div>
 
 <div class="standoff-left">
 
-    <img src="img/standoffAbout.png" alt="Book Launch Website for Bruce McIvor">
+    <img class="hidden-left" src="img/standoffAbout.png" alt="Book Launch Website for Bruce McIvor">
+
+    <p class="hidden">To keep this promise, we provide two editing sessions to guide our clients with the insight and tools needed for the greatest success. Whether you are an artist or a B2C company, you''re gonna love the results.</p>
 
 </div>
 
