88085e93985afdcaaa4690067a4351bfad1bc8d7
Samuel Phillips <codedchrysalis@gmail.com>
2022-12-15 14:48:59 -0800

    added Korochka case page

---
+++ korochka_case.php
@@ -0,0 +1,75 @@
+<?php include "includes/header.php"; ?>
+
+<div class="case-container">
+
+    <div class="case-card">
+
+        <div class="card-image-1" title="Standoff Book Release Cover Art"></div>
+
+        <div class="case-content">
+
+            <h2>Standoff: Book Release New Design</h2>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti fuga doloribus voluptates dolore. Quis vitae, aliquam incidunt corrupti, cupiditate dolorem quasi
+
+                <span class="read-more-text">minus tempora molestias a ipsam necessitatibus quaerat, asperiores ipsa cum perspiciatis atque saepe sequi.
+                </span>
+            </p>
+
+            <span class="read-more-btn">read more</span>
+
+        </div>
+
+        <button onclick="document.location='standoff_case.php'">learn more</button>
+
+    </div>
+
+    <div class="case-card">
+
+        <div class="card-image-2" title="Transfroming Cities from Within Logo"></div>
+
+        <div class="case-content">
+
+            <h2>Transforming Cities from Within Design</h2>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis labore amet harum hic facilis dolorem dolores? Similique dolore voluptatem aperiam amet hic
+
+                <span class="read-more-text"> voluptatibus, quibusdam deserunt magnam, nobis numquam accusamus praesentium recusandae suscipit explicabo iusto alias ex molestias quod aut. Autem?
+                </span>
+
+            </p>
+
+            <span class="read-more-btn">read more</span>
+
+        </div>
+
+        <button onclick="document.location='tcfw_case.php'">learn more</button>
+
+    </div>
+
+    <div class="case-card">
+
+        <div class="design-img" title="a Cascade of Standoff Book Design Frames"></div>
+
+        <div class="case-content">
+
+            <h2>Standoff: Book Release New Design</h2>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti fuga doloribus voluptates dolore. Quis vitae, aliquam incidunt corrupti, cupiditate dolorem quasi
+
+                <span class="read-more-text">minus tempora molestias a ipsam necessitatibus quaerat, asperiores ipsa cum perspiciatis atque saepe sequi.
+                </span>
+            </p>
+
+            <span class="read-more-btn">read more</span>
+
+        </div>
+
+        <button onclick="document.location='standoff.php'">learn more</button>
+
+    </div>
+
+</div>
+
+
+<?php include "includes/footer.php"; ?>
