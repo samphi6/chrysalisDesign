dc9f772fdbf89a17172cfde9ac2f9bf2bdff1013
Samuel Phillips <codedchrysalis@gmail.com>
2022-12-15 14:50:35 -0800

    added Standoff design page

---
+++ standoff_case.php
@@ -0,0 +1,76 @@
+<?php include "includes/header.php"; ?>
+
+<div class="case-container">
+
+    <div class="case-card">
+
+        <div class="design-img" title="a Cascade of Standoff Book Design Frames"></div>
+
+        <div class="case-content">
+
+            <h2>Standoff: Book Release New Design</h2>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti fuga doloribus voluptates dolore. Quis vitae, aliquam incidunt corrupti, cupiditate dolorem quasi
+
+                <span class="read-more-text">minus tempora molestias a ipsam necessitatibus quaerat, asperiores ipsa cum perspiciatis atque saepe sequi.
+                </span>
+            </p>
+
+            <span class="read-more-btn">read more</span>
+
+        </div>
+
+        <button onclick="document.location='standoff.php'">learn more</button>
+
+    </div>
+
+    <div class="case-card">
+
+        <div class="card-image-2" title="Transfroming Cities from Within Logo"></div>
+
+        <div class="case-content">
+
+            <h2>Transforming Cities from Within Design</h2>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis labore amet harum hic facilis dolorem dolores? Similique dolore voluptatem aperiam amet hic
+
+                <span class="read-more-text"> voluptatibus, quibusdam deserunt magnam, nobis numquam accusamus praesentium recusandae suscipit explicabo iusto alias ex molestias quod aut. Autem?
+                </span>
+
+            </p>
+
+            <span class="read-more-btn">read more</span>
+
+        </div>
+
+        <button onclick="document.location='tcfw_case.php'">learn more</button>
+
+    </div>
+
+    <div class="case-card">
+
+        <div class="card-image-3" title="Korochka Tavern Site Design Notes"></div>
+
+        <div class="case-content">
+
+            <h2>Standoff: Book Release New Design</h2>
+
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus quisquam delectus incidunt iure veritatis sunt
+
+                <span class="read-more-text">quo deserunt eum doloremque quod.
+                </span>
+
+            </p>
+
+            <span class="read-more-btn">read more</span>
+
+        </div>
+
+        <button onclick="document.location='korochka_case.php'">learn more</button>
+
+    </div>
+
+</div>
+
+
+<?php include "includes/footer.php"; ?>
