cc5bd0e7510f6059e587a4c86887a429241cd7fa
Samuel Phillips <codedchrysalis@gmail.com>
2022-12-17 12:38:51 -0800

    added font awesome script

--- includes/header.php
+++ includes/header.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="scss/styles.css?<?php echo time(); ?>" />
     <title><?= $title ?></title>
+    <script src="https://kit.fontawesome.com/0d8d08edeb.js" crossorigin="anonymous"></script>
     <script defer src="/chrysalisDesign/js/script.js"></script>
 
 </head>
